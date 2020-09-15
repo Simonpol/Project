8cfa217a8a86892fade9ba134b72510067e22d52
juke <den_lupanov@mail.ru>
2020-09-15 10:33:52 +0300

    Web-site witl SkillsAnimation V1.1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 $(document).ready(function(){
-	$('.header__burger').click(function(event){
-		$('.header__burger,.header__menu').toggleClass('active');
-		$('body').toggleClass('lock');
+	// Меню сверху становится черным при скроллинге
+	$(window).scroll(function(){
+		$(".header").css({
+			"background-color":"black",
+			"transition":"1s",
+		});
+	})
+	// Заливка при наведении курсора на кнопку
+	$("#btn").hover(function(){
+		$("#btn").css({
+			"background-color": "white",
+			"color": "black",
+			"border-color": "orange",
+		});
+	},
+	function(){
+		$("#btn").css({
+			"background-color": "transparent",
+			"color":"white",
+			"border-color": "white",
+		});
 	});
-});
+	// Прокрутка до блока "Какие задачи я выполняю"
+	var butn = document.getElementById("btn");
+	var dp = document.getElementById("skills");
+
+	function scrollInSkills(){
+		dp.scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	}
+	butn.addEventListener("click", scrollInSkills)
+	// Конец прокрутки
+
+	
+
+
+
+})
